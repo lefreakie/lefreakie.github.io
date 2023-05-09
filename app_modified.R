cf002ec5c5b1eb111aeff4399f0a5fd905395c81
microhexa <106036921+microhexa@users.noreply.github.com>
2023-05-09 12:18:26 +0200

    Udkast til ny udseende

---
+++ app_modified.R
@@ -0,0 +1,112 @@
+source("setup.R")
+
+results_list <- read_rds("data/full_regression_list.Rds")
+all_list_names <- read_from_excel("data/all_list_names.xlsx", T)
+all_list_names_no_baseline <- read_rds("data/all_list_no_names.Rds")
+prs_inventoried <- read_from_excel("data/prs_inventoried.xlsx", T)
+
+hash_table <- read_rds("data/hash_table.Rds")
+groupings_table <- read_rds("data/groupings_table.Rds")
+
+ui <- fluidPage(
+  
+  titlePanel(h1("Prediction of readmissions with Anorexia Nervosa", style = {'background-color: #092052; color: white; padding: 10px'})),
+  
+  sidebarLayout(
+    sidebarPanel(
+      tags$h3("Choice of variable"),
+      tags$hr(style = {'margin: 0 0 20px 0;'}),
+      checkboxGroupInput(inputId = "checkbox",
+                         label = NULL,
+                         choiceValues = names(all_list_names_no_baseline),
+                         choiceNames = names(all_list_names_no_baseline)
+      )
+    ),
+    
+    mainPanel(
+      fluidRow(
+        column(6,
+               verbatimTextOutput("vars"),
+               plotOutput("plot", height = "700px")
+        ),
+        column(6,
+               tableOutput("table")
+        )
+      )
+    )
+  )
+  
+)
+
+
+server <- function(input, output){
+
+    selected_dataset <- reactive({
+#    return (eval(parse(text = input$dataset)))
+      hash_table[[input$checkbox]]
+        }
+    )
+    
+    map_to_variable2 <- function(variables){case_when(
+      variables %in% prs_inventoried$behavior ~"Behavior",
+      variables %in% prs_inventoried$lifestyle ~"Lifestyle",
+      variables %in% prs_inventoried$health ~"Health",
+      variables %in% prs_inventoried$mental ~"Mental",
+      variables %in% prs_inventoried$congenital ~"Congenital",
+      variables %in% all_list_names$mother ~"Mother",
+      variables %in% all_list_names$father ~"Father",
+      variables %in% all_list_names$educ ~"Education",
+      variables %in% all_list_names$income ~"Income",
+      variables %in% all_list_names$birth ~"Birth",
+      variables %in% all_list_names$civ_stat ~"Civil_Status",
+      variables %in% all_list_names$baseline ~"Baseline",
+      variables %in% all_list_names$PC ~"Principal Components",
+      variables %in% prs_inventoried$ld_pred ~"ldPred",
+      variables %in% all_list_names$patient ~"Patient mental illness"
+    )}
+    
+#    output$vars <- renderPrint(hash_table[[input$checkbox]])
+    
+    
+    map_to_variable <- function(variables){
+      if(!is_null(groupings_table[[variables]])) groupings_table[[variables]]
+      else groupings_table[[str_replace(variables, "(.*[a-z|A-Z]+)[0-9]$", "\\1")]]
+    }
+    
+    output$table <- renderTable({
+        selected_dataset() %>%
+        rowwise(variables) %>%
+            mutate(group_names = map_to_variable(variables)) %>%
+        ungroup()
+    })
+  
+    outputted_dataset <- function(){
+      selected_dataset() %>%
+        rowwise(variables) %>%
+        mutate(group_names = as.factor(map_to_variable(variables))) %>%
+        ungroup()
+    }
+    
+#    max_coef <- max(selected_dataset %>% ds))
+    
+    make_factor_vars <- function(vars, groups){
+      factor(interaction(vars, groups, drop = T), labels = vars)
+    }
+    
+    output$plot <- renderPlot({
+            outputted_dataset() %>%
+#            arrange(-group_names, -variables) %>%
+            ggplot(aes(y = factor(interaction(variables, group_names, drop = T), labels = variables), group = group_names, color = group_names)) + 
+            theme_classic() +
+            theme(legend.position = c(.9, .9)) +
+            ylab("") +
+            geom_point(aes(x = `exp(coef)`), shape = 15, size = 1) +
+            geom_errorbarh(aes(xmin = `lower .95`, xmax = `upper .95`), height = .2, size = 1) +
+            geom_linerange(aes(xmin = `lower .95`, xmax = `upper .95`)) +
+            geom_vline(xintercept = 1, linetype = "dashed") +
+            xlim(0, 3)
+    })
+    
+}
+
+shinyApp(ui, server)
